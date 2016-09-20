02dd39f07ac55aeca4094551c25bbeca4656e5fd
Rubén Rodríguez <ruben.rodriguez@interactiv4.com>
2016-09-20 17:12:05 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# magento1-gulpfile-boilerplate
+# Magento 1 Gulpfile Boilerplate
+A gulpfile boilerplate for Magento 1.x projects
+
+## Getting Started
+
+1. Setting the correct routes
+- magento.name : Define the project name
+- magento.pkg : Define your package
+- magento.theme : Define your theme
+- magento.domain : Define your domain
+- magento.skin : Define the skin route
+- magento.src : Define de SASS route folder
+- magento.dest : Define the destination CSS folder
+- magento.js : Define the JS route folder
+
+## Gulp Tasks
+
+1. Default: "gulp"
+Compile and minify all the SASS files
+2. Build: "gulp build"
+Compile, minify all de SASS files and minify the JS using requireJS Optimizer
+3. SASS compilation: "gulp sass"
+Compile the SASS files
+4. SASS compile and sync "gulp watch"
+Compile and reload browser styles using browserSync. Pay attention to the magento.domain var.
+5. SASS minify "gulp minify"
+Minify all de CSS
+6. JS minify "gulp scripts"
+Minify all the .js scripts using requireJS Optimizer
---
+++ gulpfile.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var gulp         = require('gulp');
+
+// Gulp Plugins
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync  = require('browser-sync');
+var notify       = require('gulp-notify');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var shell = require('gulp-shell');
+
+// Magento Settings
+var magento = {};
+
+magento.name = 'project-name'; // Define the project name
+magento.pkg = 'package'; // Define your package
+magento.theme = 'theme'; // Define your theme
+magento.domain = 'local.domain.com'; // Define your domain
+
+//If you set the gulpfile in Magento root
+//magento.skin = './skin/frontend/';
+//magento.src = magento.skin + magento.pkg + '/' + magento.theme + '/scss/**/*.scss';
+//magento.dest = magento.skin + magento.pkg + '/' + magento.theme + '/css/';
+
+//If you set the gulpfile in skin template folder
+magento.skin = './' + magento.name;
+magento.src = 'scss/**/*.scss';
+magento.dest = 'css/';
+magento.js ='js/';
+
+//Sync with browsersync
+gulp.task('sync', function() {
+    browserSync.init([
+        magento.dest + '*.css'
+    ], {
+        host: magento.domain,
+        proxy: magento.domain,
+        ui: false,
+        open: false
+    });
+});
+
+//Compile SASS
+gulp.task('sass', function() {
+    return gulp.src(magento.src)
+        .pipe(sass({includePaths: 'bower_components/compass-mixins/lib',
+            outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer({ browsers: ['last 2 version', 'safari 5', 'ie >= 9'] }))
+        .pipe(gulp.dest(magento.dest))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe(notify({message: magento.name + ' compiled successfully', onLast: true}));
+});
+
+//Minify SASS
+gulp.task('minify', function () {
+    gulp.src(magento.dest + '*.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS(
+            {
+                recursivelyOptimizeProperties: true
+            }
+        ))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(magento.dest))
+        .pipe(notify({message: magento.name + ' minified successfully', onLast: true}));
+    ;
+});
+
+//Compile and sync with browserSync
+gulp.task('watch', ['sync'], function() {
+    gulp.watch(magento.src, ['sass']);
+});
+
+//Minify all the js
+gulp.task('scripts', shell.task([
+    // This is the command
+    'r.js -o ' + magento.js + 'build.js'
+]));
+
+//Default task: Only compile and minify CSS
+gulp.task('default', ['sass', 'minify']);
+
+//Build task for production mode: Compile, minify and generate CSS and JS
+gulp.task('build', [
+    'sass',
+    'minify',
+    'scripts',
+]);
